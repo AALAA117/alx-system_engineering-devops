6267e2d525f41b6c733192ad91554ee9bae8ad35
alaa <alaamglal@gmail.com>
2024-06-18 18:31:20 -0700

    done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 """
 prints the titles of the first 10 hot posts listed for a given subreddit
 """
-
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
@@ -12,22 +10,18 @@ def top_ten(subreddit):
     function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
+    headers = {'User-Agent': 'Mozilla/5.0'}
     try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            print(f"Top 10 posts in r/{subreddit}:\n")
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+        else:
+            print("None")
+    except requests.exceptions.RequestException as e:
         print("None")
